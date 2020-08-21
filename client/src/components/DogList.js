6ca65710389ad7fb7e562d6f3b5895a1511cb598
mikefallon <mikefallon81@gmail.com>
2020-08-21 10:17:56 +0100

    DogList css imported

--- client/src/components/DogList.js
+++ client/src/components/DogList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import DogItem from "./DogItem";
+import "./DogList.css";
 
 const DogList = ({ dogs, onDogSelect }) => {
   const renderedList = dogs.map((dog) => {
     return <DogItem key={dog.name} dog={dog} onDogSelect={onDogSelect} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="renderedList">{renderedList}</div>;
 };
 
 export default DogList;
